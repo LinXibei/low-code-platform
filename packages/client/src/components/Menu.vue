a6cd9a33fcb4a5a6e29da2e77f6d854160a67c13
linyuanyuan <herryleehom@126.com>
2024-01-10 13:34:48 +0800

    feat: updated menu css

--- packages/client/src/components/Menu.vue
+++ packages/client/src/components/Menu.vue
@@ -27,17 +27,17 @@ const isFold = ref(false)
         @close="handleClose"
         :router="true"
       >
-        <el-menu-item index="/home">
+        <el-menu-item index="/home" class="menu-item-custom">
           <el-icon><location /></el-icon>
-          <template #title>Home</template>
+          <template #title><span>Home</span></template>
         </el-menu-item>
-        <el-menu-item index="/login">
+        <el-menu-item index="/login" class="menu-item-custom">
           <el-icon><icon-menu /></el-icon>
-          <template #title>Login</template>
+          <template #title><span>Login</span></template>
         </el-menu-item>
-        <el-menu-item index="/editor">
+        <el-menu-item index="/editor" class="menu-item-custom">
           <el-icon><document /></el-icon>
-          <template #title>Editor</template>
+          <template #title><span>Editor</span></template>
         </el-menu-item>
         <el-menu-item index="4">
           <el-icon><setting /></el-icon>
@@ -46,7 +46,7 @@ const isFold = ref(false)
       </el-menu>
     </div>
     <div class="menu-fold">
-      <span @click="isFold = !isFold"><el-icon size="22"><Fold /></el-icon></span>
+      <span @click="isFold = !isFold"><el-icon size="22" class="icon-custom"><Fold /></el-icon></span>
     </div>
   </div>
 </template>
@@ -56,6 +56,9 @@ const isFold = ref(false)
   width: 200px;
   height: 100%;
   background-color: #fff;
+  z-index: 2;
+  position: relative;
+  box-shadow: 0px 2px 8px rgba(167, 199, 233, 0.2);
   .menu-vertical-container{
     height: calc(100vh - 120px);
     overflow-y: auto;
@@ -64,14 +67,73 @@ const isFold = ref(false)
   .menu-fold{
     height: 60px;
     line-height: 60px;
+    text-align: left;
     span{
       padding: 10px;
       color: #303133;
       cursor: pointer;
+      border-radius: 4px;
       &:hover{
         background: #f5f6f9;
       }
+      .icon-custom{
+        position: relative;
+        top: 4px;
+      }
     }
+  }
+  .menu-item-custom{
+    height: 48px;
+    margin-bottom: 20px;
+    position: relative;
+    span{
+      position: relative;
+    }
+    &:hover{
+      background-color: #fff;
+      &:before{
+        visibility: visible;
+      }
+    }
+    &:before{
+      content: "";
+      position: absolute;
+      left: 0;
+      top: 0;
+      background: #f5f6f9;
+      border-radius: 4px;
+      width: 180px;
+      left: 10px;
+      height: 48px;
+      visibility: hidden;
+    }
+  }
+}
+</style>
+<style lang="scss">
+.menu-vertical-container .el-menu{
+  border-right: none;
+}
+.menu-vertical-container .el-menu-item{
+  border-radius: 4px;
+  padding: 5px;
+}
+.menu-vertical-container .el-menu-item.is-active.menu-item-custom{
+  color: #409eff;
+  &:before{
+    background: rgba( 64, 158, 255 , .1);
+    visibility: visible;
+  }
+  &:after{
+    background: #147bd1;
+    content: "";
+    display: inline-block;
+    height: 30px;
+    width: 4px;
+    position: absolute;
+    left: 0;
+    top: 9px;
+    border-radius: 0 8px 8px 0;
   }
 }
 </style>
