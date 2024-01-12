816b0f771a09cd72eb5813a54b5b9075b9939a41
linyuanyuan <herryleehom@126.com>
2024-01-12 15:00:56 +0800

    feat: add related icon and font

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -1,11 +1,34 @@
 import { createApp } from 'vue'
 import './styles/reset.css'
 import './style.css'
+import '@/assets/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import registerElpComp from './utils/registerElpComp'
+// 创建fontawesome字体（主要用于拖拽组件的icon）
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { 
+  faCode,
+  faEye,
+  faFileExport,
+  faTrashRestoreAlt,
+  faFile,
+  faTree,
+  faFileArchive
+} from '@fortawesome/free-solid-svg-icons'
+library.add(faCode);
+library.add(faEye);
+library.add(faFileExport);
+library.add(faTrashRestoreAlt);
+library.add(faFile);
+library.add(faTree);
+library.add(faFileArchive);
+
 const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+
 registerElpComp(app)
 app.use(store).use(router)
 app.mount('#app', true)
